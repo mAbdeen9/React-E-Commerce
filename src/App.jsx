4a33eecdcad36b285fccc22ba1202c77c43bf29e
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2022-10-09 23:42:18 +0300

    mobile header

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className="test"></div>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,94 @@
 import React, { Fragment, useEffect, useState } from "react";
+import {
+  List,
+  Cart,
+  Person,
+  Search,
+  GeoAlt,
+  HouseDoor,
+  XLg,
+} from "react-bootstrap-icons";
 import classes from "./Header.module.css";
-
-import { List, Cart, Person, Search, GeoAlt } from "react-bootstrap-icons";
 import axios from "axios";
 
 function Header() {
   const [country, setCountry] = useState();
+  const [mobileNavClass, setMobileNavClass] = useState("none");
+
+  const mobileNavHandler = () => {
+    setMobileNavClass((state) => {
+      document.body.style.overflow = "hidden";
+      state = "";
+      return state;
+    });
+  };
+
+  const mobileCloseNavHandler = () => {
+    setMobileNavClass((state) => {
+      document.body.style.overflow = "unset";
+      state = "none";
+      return state;
+    });
+  };
 
   useEffect(() => {
     const getGeoInfo = async () => {
-      const data = await axios.get("https://ipapi.co/json/");
-      setCountry(data.data.city);
+      try {
+        const data = await axios.get("https://ipapi.co/json/");
+        setCountry(data.data.city);
+      } catch (err) {
+        alert(err);
+      }
     };
     getGeoInfo();
-  });
+  }, []);
 
   return (
     <Fragment>
       {/* -- Start Mobile -- */}
+      <div
+        style={{ display: mobileNavClass }}
+        className={classes.mobile__sidenav}
+      >
+        <div className={classes.rightbar}>
+          <div className={classes.rightbar__top}>
+            <div className={classes.mobile__sign}>
+              <span>Sign in </span>
+              <Person color="white" size={"25px"} />
+            </div>
+            <div className={classes.rightbar__head}>
+              <div>Browse</div>
+              <div>Shopzon</div>
+            </div>
+          </div>
+          <div className={classes.rightbar__rest}>
+            <div className={classes.flex__list}>
+              <div>Shopzon Home</div>
+              <div>
+                <HouseDoor fontWeight="bold" size="24px" />
+              </div>
+            </div>
+            <div className={classes.line}></div>
+            <div className={classes.flex__listY}>
+              <div>Trending</div>
+              <p>Best Sellers</p>
+              <p>New Releases</p>
+            </div>
+            <div className={classes.line}></div>
+            <div className={classes.flex__listY}>
+              <div>About us </div>
+              <p>Team</p>
+              <p>Technologies</p>
+            </div>
+          </div>
+        </div>
+        <div onClick={mobileCloseNavHandler} className={classes.leftbar}>
+          <XLg color="white" fontWeight="bold" size="30px" />
+        </div>
+      </div>
       <div className={classes.header__mobile}>
         <div>
-          <span className={classes.mobile__burger}>
+          <span onClick={mobileNavHandler} className={classes.mobile__burger}>
             <List color="white" size={"35px"} />
           </span>
           <span className={classes.logo}>
@@ -51,7 +119,6 @@ function Header() {
         <GeoAlt color="white" /> <span> Deliver to {country}</span>
       </div>
 
-      <div className={classes.mobile__sidenav}></div>
       {/* -- END Mobile -- */}
     </Fragment>
   );
