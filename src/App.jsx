f11641f0de0a15a782e7eac56c82578c9464b5a6
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2022-10-13 20:00:20 +0300

    adding big screen css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
-import SignUp from "./components/Sign-up/SignUp";
-// import Header from "./components/Header/Header";
+// import SignUp from "./components/Sign-up/SignUp";
+import Header from "./components/Header/Header";
 // import Signin from "./components/Sign-in/Signin";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header />
-      <div className="test"></div> */}
-      <SignUp />
+      <Header></Header>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,6 +7,7 @@ import {
   GeoAlt,
   HouseDoor,
   XLg,
+  ArrowDownShort,
 } from "react-bootstrap-icons";
 import classes from "./Header.module.css";
 import axios from "axios";
@@ -46,80 +47,121 @@ function Header() {
   return (
     <Fragment>
       {/* -- Start Mobile -- */}
-      <div
-        style={{ display: mobileNavClass }}
-        className={classes.mobile__sidenav}
-      >
-        <div className={classes.rightbar}>
-          <div className={classes.rightbar__top}>
-            <div className={classes.mobile__sign}>
-              <span>Sign in </span>
-              <Person color="white" size={"25px"} />
-            </div>
-            <div className={classes.rightbar__head}>
-              <div>Browse</div>
-              <div>Shopzon</div>
-            </div>
-          </div>
-          <div className={classes.rightbar__rest}>
-            <div className={classes.flex__list}>
-              <div>Shopzon Home</div>
-              <div>
-                <HouseDoor fontWeight="bold" size="24px" />
+      <div className={classes.mobile__header}>
+        <div
+          style={{ display: mobileNavClass }}
+          className={classes.mobile__sidenav}
+        >
+          <div className={classes.rightbar}>
+            <div className={classes.rightbar__top}>
+              <div className={classes.mobile__sign}>
+                <span>Sign in </span>
+                <Person color="white" size={"25px"} />
+              </div>
+              <div className={classes.rightbar__head}>
+                <div>Browse</div>
+                <div>Shopzon</div>
               </div>
             </div>
-            <div className={classes.line}></div>
-            <div className={classes.flex__listY}>
-              <div>Trending</div>
-              <p>Best Sellers</p>
-              <p>New Releases</p>
-            </div>
-            <div className={classes.line}></div>
-            <div className={classes.flex__listY}>
-              <div>About us </div>
-              <p>Team</p>
-              <p>Technologies</p>
+            <div className={classes.rightbar__rest}>
+              <div className={classes.flex__list}>
+                <div>Shopzon Home</div>
+                <div>
+                  <HouseDoor fontWeight="bold" size="24px" />
+                </div>
+              </div>
+              <div className={classes.line}></div>
+              <div className={classes.flex__listY}>
+                <div>Trending</div>
+                <p>Best Sellers</p>
+                <p>New Releases</p>
+              </div>
+              <div className={classes.line}></div>
+              <div className={classes.flex__listY}>
+                <div>About us </div>
+                <p>Team</p>
+                <p>Technologies</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div onClick={mobileCloseNavHandler} className={classes.leftbar}>
-          <XLg color="white" fontWeight="bold" size="30px" />
-        </div>
-      </div>
-      <div className={classes.header__mobile}>
-        <div>
-          <span onClick={mobileNavHandler} className={classes.mobile__burger}>
-            <List color="white" size={"35px"} />
-          </span>
-          <span className={classes.logo}>
-            Shop<span style={{ color: "#ff9900" }}>zon</span>
-          </span>
-        </div>
-        <div className={classes.mobile__right}>
-          <div className={classes.mobile__sign}>
-            <span>Sign in ›</span>
-            <Person color="white" size={"25px"} />
+          <div onClick={mobileCloseNavHandler} className={classes.leftbar}>
+            <XLg color="white" fontWeight="bold" size="30px" />
           </div>
-          <span className={classes.mobile__cart}>
-            <Cart color="white" size={"24px"} />
-          </span>
+        </div>
+        <div className={classes.header__mobile}>
+          <div>
+            <span onClick={mobileNavHandler} className={classes.mobile__burger}>
+              <List color="white" size={"35px"} />
+            </span>
+            <span className={classes.logo}>
+              Shop<span style={{ color: "#ff9900" }}>zon</span>
+            </span>
+          </div>
+          <div className={classes.mobile__right}>
+            <div className={classes.mobile__sign}>
+              <span>Sign in ›</span>
+              <Person color="white" size={"25px"} />
+            </div>
+            <span className={classes.mobile__cart}>
+              <Cart color="white" size={"24px"} />
+            </span>
+          </div>
+        </div>
+
+        <form className={classes.mobile__form}>
+          <div className={classes.mobile__serachbar}>
+            <input type="text" placeholder="Search Shopzon" />
+            <button>
+              <Search color="black" size={"25px"} fontWeight="bold" />
+            </button>
+          </div>
+        </form>
+
+        <div className={classes.mobile__location}>
+          <GeoAlt color="white" /> <span> Deliver to {country}</span>
         </div>
       </div>
-
-      <form className={classes.mobile__form}>
-        <div className={classes.mobile__serachbar}>
-          <input type="text" placeholder="Search Shopzon" />
-          <button>
-            <Search color="black" size={"25px"} fontWeight="bold" />
-          </button>
-        </div>
-      </form>
-
-      <div className={classes.mobile__location}>
-        <GeoAlt color="white" /> <span> Deliver to {country}</span>
-      </div>
-
       {/* -- END Mobile -- */}
+      {/* Start Big Screens Header */}
+      <div className={classes.bg__header}>
+        <div className={classes.bg__top}>
+          <div className={classes.bg__logo}>
+            <span className={classes.logo}>
+              Shop<span style={{ color: "#ff9900" }}>zon</span>
+            </span>
+          </div>
+          <div className={classes.bg__deliver}>
+            <div>
+              <GeoAlt fontWeight="bold" fontSize="20px" />
+            </div>
+            <div>
+              <span className={classes.bg__deliver__hello}>Hello</span>
+              <div>
+                <span>Deliver to {country}</span>
+              </div>
+            </div>
+          </div>
+          <div className={classes.bg__search}>
+            <form>
+              <div className={classes.bg__serachbar}>
+                <input type="text" />
+                <button>
+                  <Search color="black" size={"20px"} fontWeight="bold" />
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className={classes.bg__hello}>
+            <div className={classes.bg__deliver__hello}>Hello, sign in</div>
+            <div>
+              Account & Lists <ArrowDownShort />{" "}
+            </div>
+          </div>
+          <div className={classes.bg__cart}>
+            <Cart color="white" fontSize="30px" /> <span>Cart</span>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
