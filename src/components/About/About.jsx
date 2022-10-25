223a68372ecc3cd704b8714feeb0a4a4b3be8844
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2022-10-25 22:17:50 +0300

    adding about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,11 +6,15 @@ function About() {
     <div className={classes.about__box}>
       <div className={`container ${classes.cont}`}>
         <div>
-          Shop<span style={{ color: "#ff9900", fontWeight: "bold" }}>zon</span>{" "}
-          is a dummy Full-Stack E-commerce web app built with MongoDB, Express,
-          Redux , React, and Node.js , this app implement all the CRUD
-          operations , manipulate the cart , sign in / up , reset password and
-          more.
+          <h1>About</h1>
+          <p>
+            Shop
+            <span style={{ color: "#ff9900", fontWeight: "bold" }}>zon</span> is
+            a dummy Full-Stack E-commerce web app built with MongoDB, Express,
+            Redux , React, and Node.js , this app implement all the CRUD
+            operations , manipulate the cart , sign in / up , reset password and
+            more.
+          </p>
         </div>
         <img src={aboutImg} alt="about" />
       </div>
