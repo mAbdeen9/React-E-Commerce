d2d3c539f942e0d56a1119877b79cb5704111863
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2022-11-02 16:51:05 +0200

    adding cart modal

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,73 +4,104 @@ import EmptyImgae from "../../assets/empty-cart.png";
 import { useNavigate } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../../store/CartSlice";
+import { toast } from "react-toastify";
+
 function Cart() {
   const navigate = useNavigate();
   const { cart, items, subtotal } = useSelector((state) => state.Cart);
+
   const dispatch = useDispatch();
   return (
     <div className={classes.main__box}>
-      {/* <div className={`container ${classes.flex__box}`}>
-        <img src={EmptyImgae} alt="empty cart" />
-        <div className={classes.text__box}>
-          <div>Your Amazon Cart is empty</div>
-          <div style={{ color: "#a3a5a5" }}>Good stuff goes in here</div>
-          <div onClick={() => navigate("/")}>See recommendations</div>
+      {cart.length <= 0 && (
+        <div className={`container ${classes.flex__box}`}>
+          <img src={EmptyImgae} alt="empty cart" />
+          <div className={classes.text__box}>
+            <div>Your Amazon Cart is empty</div>
+            <div style={{ color: "#a3a5a5" }}>Good stuff goes in here</div>
+            <div onClick={() => navigate("/")}>See recommendations</div>
+          </div>
         </div>
-      </div> */}
-      <div className={`container ${classes.cart__check}`}>
-        <div className={classes.orders}>
-          <div className={classes.orders__title}>
-            <div>
-              <h3>Shopping Cart</h3>
-              <span>all items</span>
+      )}
+      {cart.length >= 1 && (
+        <div className={`container ${classes.cart__check}`}>
+          <div className={classes.orders}>
+            <div className={classes.orders__title}>
+              <div>
+                <h3>Shopping Cart</h3>
+                <span>all items</span>
+              </div>
+              <div className={classes.price}>Price</div>
             </div>
-            <div className={classes.price}>Price</div>
-          </div>
-          <div className={classes.line}></div>
-          {cart?.map((product, i) => {
-            return (
-              <Fragment key={i}>
-                <div className={classes.product__card}>
-                  <img src={product.image} alt="product" />
-                  <div className={classes.product__des}>
-                    <div className={classes.card__title}>
-                      <div>{product.title}</div>
-                      <div style={{ color: "#007600", fontSize: "14px" }}>
-                        in Stock
+            <div className={classes.line}></div>
+            {cart?.map((product, i) => {
+              return (
+                <Fragment key={i}>
+                  <div className={classes.product__card}>
+                    <img src={product.image} alt="product" />
+                    <div className={classes.product__des}>
+                      <div className={classes.card__title}>
+                        <div>{product.title}</div>
+                        <div style={{ color: "#007600", fontSize: "14px" }}>
+                          in Stock
+                        </div>
+                        <div>Free Shipping</div>
                       </div>
-                      <div>Free Shipping</div>
+                      <span className={classes.qty}>Qty: {product.count}</span>
+                      <div
+                        onClick={() =>
+                          dispatch(cartActions.deleteOrderFromCart(product))
+                        }
+                        className={classes.card__delete__Btn}
+                      >
+                        Delete
+                      </div>
                     </div>
-                    <span className={classes.qty}>Qty: {product.count}</span>
-                    <div
-                      onClick={() =>
-                        dispatch(cartActions.deleteOrderFromCart(product))
-                      }
-                      className={classes.card__delete__Btn}
-                    >
-                      Delete
-                    </div>
+                    <div className={classes.card__price}>${product.price}</div>
                   </div>
-                  <div className={classes.card__price}>${product.price}</div>
-                </div>
-                <div className={classes.line}></div>
-              </Fragment>
-            );
-          })}
-          <div className={classes.subtotal}>
-            Subtotal ({items} items):
-            <span> ${subtotal.toFixed(2)}</span>
+                  <div className={classes.line}></div>
+                </Fragment>
+              );
+            })}
+            <div className={classes.subtotal}>
+              Subtotal ({items} items):
+              <span> ${subtotal.toFixed(2)}</span>
+            </div>
+          </div>
+          <div className={classes.checkout}>
+            <div className={classes.subtotalCheck}>
+              Subtotal ({items} items): <span> ${subtotal.toFixed(2)}</span>
+            </div>
+            <div
+              onClick={() => {
+                toast.warn("No Payments !", {
+                  position: "top-right",
+                  autoClose: 5000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "light",
+                });
+                toast.success("But you can hire me 😎 ", {
+                  position: "top-right",
+                  autoClose: 6000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "light",
+                });
+              }}
+              className={classes.checkout__btn}
+            >
+              <button>Proceed to checkout</button>
+            </div>
           </div>
         </div>
-        <div className={classes.checkout}>
-          <div className={classes.subtotalCheck}>
-            Subtotal ({items} items): <span> ${subtotal.toFixed(2)}</span>
-          </div>
-          <div className={classes.checkout__btn}>
-            <button>Proceed to checkout</button>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
