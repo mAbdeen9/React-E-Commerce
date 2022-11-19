0a865c9d7c89c7beb7df34c03f50b516143fbd83
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2022-11-19 19:37:56 +0200

    test

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -1,26 +1,34 @@
 import React, { Fragment } from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
 import httpRequest from "../../helpers/httpRequest";
 import { authActions } from "../../store/AuthSlice";
-
+import { Cart } from "react-bootstrap-icons";
 import classes from "./AdminPanel.module.css";
 
 function AdminPanel() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const userToken = JSON.parse(localStorage.getItem("JWT")) || null;
     const getAllCart = async () => {
+      setIsLoading(true);
       try {
         const response = await httpRequest("GET", "/cart/all-carts", userToken);
-        console.log(response);
+        const carts = response?.data?.data.filter((e) => e !== null);
+        const data = carts.map((e) => e?.cart).flat(1);
+
+        setData(data);
       } catch (err) {
-        console.log(err);
+        toast(err?.message);
       }
+      setIsLoading(false);
     };
 
     getAllCart();
@@ -50,7 +58,53 @@ function AdminPanel() {
       </div>
       <div className={classes.admin__content}>
         <div className="container">
-          <h3>Customers Cart</h3>
+          <div>
+            <h3 className={classes.cc__h}>
+              All Users Cart <Cart color="#017185" fontSize="30px" />{" "}
+            </h3>
+            <div className={classes.orders__box}>
+              <div className={classes.line}></div>
+              {isLoading
+                ? "Loading data ..."
+                : data.map((product, i) => {
+                    return (
+                      <Fragment key={i}>
+                        <div className={classes.product__card}>
+                          <img src={product.image} alt="product" />
+                          <div className={classes.product__des}>
+                            <div className={classes.card__title}>
+                              <div>{product.title}</div>
+                              <div
+                                style={{ color: "#007600", fontSize: "14px" }}
+                              >
+                                in Stock
+                              </div>
+                              <div>Free Shipping</div>
+                            </div>
+                            <span className={classes.qty}>
+                              Qty: {product.count}
+                            </span>
+                            <div
+                              onClick={() =>
+                                toast(
+                                  "Dummy panel just customers manipulate the cart ✋🏻 "
+                                )
+                              }
+                              className={classes.card__delete__Btn}
+                            >
+                              Prepare Order
+                            </div>
+                          </div>
+                          <div className={classes.card__price}>
+                            ${product.price}
+                          </div>
+                        </div>
+                        <div className={classes.line}></div>
+                      </Fragment>
+                    );
+                  })}
+            </div>
+          </div>
         </div>
       </div>
     </Fragment>
